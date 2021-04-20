dd8ca72e49dd8253941f472172681458140ec63a
Bruno Ouellet <b.ouellet2@gmail.com>
2021-04-19 22:00:02 -0400

    Bilingual support

--- src/replaceText.js
+++ src/replaceText.js
@@ -1,5 +1,11 @@
+const language = document.querySelector('html').lang;
+const santeDollar = {
+    fr: 'Santédollars',
+    en: 'Healthdollars'
+};
+
 export default function replaceText(element) {
     if (element && element.textContent) {
-        element.textContent = element.textContent.replace('Santédollars', 'Ninja Coins');
+        element.textContent = element.textContent.replace(santeDollar[language], 'Ninja Coins');
     }
 }
